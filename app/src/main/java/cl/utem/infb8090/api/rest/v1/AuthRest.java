2ed966cb306d61ab13c74c70ca3d30f1a7d5c06b
nembiven <37608579+nembiven@users.noreply.github.com>
2021-07-27 17:03:08 -0400

    Update AuthRest.java

--- app/src/main/java/cl/utem/infb8090/api/rest/v1/AuthRest.java
+++ app/src/main/java/cl/utem/infb8090/api/rest/v1/AuthRest.java
@@ -26,6 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(value = "/Auth", consumes = {"application/json;charset=utf-8"}, produces = {"application/json;charset=utf-8"})
 public class AuthRest implements Serializable {
 
