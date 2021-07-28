81f56179bd33154669bfc3df6c17617a67e279b8
nembiven <37608579+nembiven@users.noreply.github.com>
2021-07-28 00:23:08 -0400

    Update AuthRest.java

--- app/src/main/java/cl/utem/infb8090/api/rest/v1/AuthRest.java
+++ app/src/main/java/cl/utem/infb8090/api/rest/v1/AuthRest.java
@@ -48,7 +48,7 @@ public class AuthRest implements Serializable {
         @ApiResponse(code = 412, message = "Falló alguna precondición", response = ErrorVO.class)
     })
     //@CrossOrigin(origins = "*")
-    @PostMapping(value = "/", consumes = {"application/json;charset=utf-8"}, produces = {"application/json;charset=utf-8"})
+    @PostMapping(value = "/login", consumes = {"application/json;charset=utf-8"}, produces = {"application/json;charset=utf-8"})
     public ResponseEntity login(@RequestBody LoginVO request) {
         /**
          * Validar que exista un objeto de entrada
